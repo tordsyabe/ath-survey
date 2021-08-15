43adea1f17725777277cd9eb805296279285319d
John Llave <extordsllave@gmail.com>
2021-08-15 13:21:02 +0400

    added active link on nav using jquery

--- athsurveyapp/static/js/main.js
+++ athsurveyapp/static/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+  const activePage = window.location.pathname;
+
+  $(".nav-item").each(function () {
+    var linkPage = $(this).find("a").attr("href");
+    console.log(activePage, linkPage);
+    if (activePage === linkPage) {
+      $(this).addClass("active");
+    }
+  });
+
   // Bootsrap select picker
   $(".selectpicker").selectpicker();
 
